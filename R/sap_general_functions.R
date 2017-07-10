ad999f31764f863d774bc4357fa125c35330a78e
Graham Parkes <graham@theparkes.me.uk>
2017-07-10 18:57:54 +0100

    Altered quantile for more granularity

--- R/sap_general_functions.R
+++ R/sap_general_functions.R
@@ -20,7 +20,7 @@ sap_stad_quantile <- function(o,testfield,pattern,summaryfield) {
 
     testf <- which(names(o)==testfield,arr.ind=TRUE)
 
-    df <- as.data.frame(quantile(o[o[,testf] %in% pattern,summaryfield],c(0.05, 0.1,0.15,0.2,0.25,0.3,0.35,0.40,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,0.98,0.99,1)))
+    df <- as.data.frame(quantile(o[o[,testf] %in% pattern,summaryfield],seq.int(0.01,1,0.01)))
 
     names(df)[1] <- 'val'
     df$percentile <- as.numeric(str_extract(row.names(df),"[0-9]*"))
